5cdca4ca8200ed87fa34b9b8601687e0a658ca5f
zhangguowei <zhangguowei@pku.edu.cn>
2016-08-15 15:49:11 +0800

    finish get_batch_keys
    Signed-off-by: zhangguowei <zhangguowei@pku.edu.cn>

--- qconf.cc
+++ qconf.cc
@@ -22,7 +22,7 @@ Handle<Value> QConfGetConf(const Arguments& args) {
         ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
         return scope.Close(Undefined());
     }
-    //Nan::Utf8String s0(args[0]);
+
     String::Utf8Value v8Path(args[0]);
     path = *v8Path;
 
@@ -35,11 +35,47 @@ Handle<Value> QConfGetConf(const Arguments& args) {
 
     if (ret != QCONF_OK) {
         ThrowException(Exception::TypeError(String::New("Get conf failed")));
-        return scope.Close(Undefined());;
+        return scope.Close(Undefined());
     }
     return scope.Close(String::New(value));
 }
 
+Handle<Value> QConf_get_batch_keys(const Arguments& args) {
+    HandleScope scope;
+
+    const char *path = NULL;
+    const char *idc = NULL;
+    string_vector_t keys;
+    int ret;
+
+    if (args.Length() < 1) {
+        ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
+        return scope.Close(Undefined());
+    }
+
+    String::Utf8Value v8Path(args[0]);
+    path = *v8Path;
+
+    if (args.Length() >= 2) {
+        String::Utf8Value v8Idc(args[1]);
+        idc = *v8Idc;
+    }
+
+    init_string_vector(&keys);
+    ret = qconf_get_batch_keys(path, &keys, idc);
+
+    if (ret != QCONF_OK) {
+        ThrowException(Exception::TypeError(String::New("Get children failed")));
+        return scope.Close(Undefined());
+    }
+
+    Handle<Array> v8Arr = Array::New(keys.count);
+    for (int i = 0; i < keys.count; ++i) {
+        v8Arr->Set(i, String::New(keys.data[i]));
+    }
+    destroy_string_vector(&keys);
+    return scope.Close(v8Arr);
+}
 
 void init(Handle<Object> target) {
     target->Set(String::NewSymbol("version"),
@@ -47,5 +83,8 @@ void init(Handle<Object> target) {
 
     target->Set(String::NewSymbol("get_conf"),
             FunctionTemplate::New(QConfGetConf)->GetFunction());
+
+    target->Set(String::NewSymbol("get_batch_keys"),
+            FunctionTemplate::New(QConf_get_batch_keys)->GetFunction());
 }
 NODE_MODULE(qconf, init)
--- test.js
+++ test.js
@@ -3,3 +3,6 @@ console.log(version);
 
 var conf = require('./build/Release/qconf.node').get_conf('/qconf/demo');
 console.log(conf);
+
+var children = require('./build/Release/qconf.node').get_batch_keys('/qconf/demo');
+console.log(children);
