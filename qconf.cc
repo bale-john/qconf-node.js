6bcee193259be50cabdf1af16f7a12968a70b32c
zhangguowei <zhangguowei@pku.edu.cn>
2016-08-15 14:54:06 +0800

    fix bugs
    Signed-off-by: zhangguowei <zhangguowei@pku.edu.cn>

--- qconf.cc
+++ qconf.cc
@@ -9,6 +9,7 @@ Handle<Value> QConfVersion(const Arguments& args) {
     HandleScope scope;
     return scope.Close(String::New(QCONF_DRIVER_CC_VERSION));
 }
+
 Handle<Value> QConfGetConf(const Arguments& args) {
     HandleScope scope;
 
@@ -16,31 +17,27 @@ Handle<Value> QConfGetConf(const Arguments& args) {
     const char *idc = NULL;
     char value[QCONF_CONF_BUF_MAX_LEN];
     int ret;
-/*
-    if (info.Length() < 1) {
-        Nan::ThrowTypeError("Wrong number of arguments: at least 1");
-        return;
+
+    if (args.Length() < 1) {
+        ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
+        return scope.Close(Undefined());
     }
-*/
-    Nan::Utf8String s0(args[0]);
-    path = *s0;
+    //Nan::Utf8String s0(args[0]);
+    String::Utf8Value v8Path(args[0]);
+    path = *v8Path;
 
     if (args.Length() >= 2) {
-        Nan::Utf8String s1(args[1]);
-        idc = *s1;
+        String::Utf8Value v8Idc(args[1]);
+        idc = *v8Idc;
     }
 
     ret = qconf_get_conf(path, value, sizeof(value), idc);
-    return scope.Close(String::New(value));
 
-    /*
     if (ret != QCONF_OK) {
-        info.GetReturnValue().Set(Nan::Null());
-        return;
+        ThrowException(Exception::TypeError(String::New("Get conf failed")));
+        return scope.Close(Undefined());;
     }
-
-    info.GetReturnValue().Set(Nan::New(value).ToLocalChecked());
-    */
+    return scope.Close(String::New(value));
 }
 
 
